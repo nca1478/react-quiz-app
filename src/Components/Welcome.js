085f9e0cb4c268ca3787e36351a431ae53fd9a7e
Nelson Cadenas <nca1478@yahoo.com>
2021-07-12 21:12:42 -0400

    added: Welcome Component

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Welcome = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="col-auto">
+      <div className="card">
+        <div className="card-header bg-primary text-light text-center">
+          <span className="h3">Welcome to Quiz</span>
+        </div>
+        <div className="card-body d-flex flex-column justify-content-center align-items-center">
+          <h5 className="card-title">
+            This is a quiz application built using ReactJS and Bootstrat 5.
+          </h5>
+          <p className="card-text">
+            It will dynamically load the question/answers pair and upload them
+            into the components.
+          </p>
+          <Link to="/config" className="btn btn-dark">
+            Config the Quiz
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
